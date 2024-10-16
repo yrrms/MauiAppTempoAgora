db5ed68d9ff71af491d6ae956f83c8578823564d
0200832211036 <0200832211036@LAB03-20>
2024-10-16 19:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/Models/Tempo.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiAppTempoAgora.Models
+{
+    public class Tempo
+    {
+        public string? Title { get; set; }//a interrogação é pra falar que pode ser nulo
+        public string? Temperature { get; set; }
+        public string? Wind { get; set; }
+        public string? Humidity {get; set;}
+        public string? Visibility { get; set; }
+        public string? Sunrise { get; set; }
+        public string? Sunset { get; set; }
+        public string? Weather { get; set; }
+        public string? WeatherDescription { get; set; }
+
+    }//fim da classe
+}//fim do namespace
